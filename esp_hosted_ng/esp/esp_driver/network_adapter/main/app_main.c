62a88090afd12231eeb29b58cca533d1ed830082
Iván Vidal <ivan@helloambient.com>
2025-03-06 13:40:13 +0100

    feat(setup): Added readme and base code example

--- esp_hosted_ng/esp/esp_driver/network_adapter/main/app_main.c
+++ esp_hosted_ng/esp/esp_driver/network_adapter/main/app_main.c
@@ -42,11 +42,89 @@
 #include "driver/uart.h"
 #endif
 #endif
+
 #include "endian.h"
 
 #include "slave_bt.c"
 #include "stats.h"
 #include "esp_mac.h"
+
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "touch_element/touch_slider.h"
+#include "esp_log.h"
+
+static const char *TAG_Slider = "Touch Slider Example";
+#define TOUCH_SLIDER_CHANNEL_NUM     6
+
+static touch_slider_handle_t slider_handle; //Touch slider handle
+
+static const touch_pad_t channel_array[] = {
+    TOUCH_PAD_NUM2,
+    TOUCH_PAD_NUM3,
+    TOUCH_PAD_NUM4,
+    TOUCH_PAD_NUM5,
+    TOUCH_PAD_NUM6,
+    TOUCH_PAD_NUM7
+};
+
+static const float channel_sens_array[] = {
+    0.015,
+    0.015,
+    0.015,
+    0.015,
+    0.015,
+    0.015
+};
+
+
+/*#define CONFIG_TOUCH_ELEM_EVENT*/
+#define CONFIG_TOUCH_ELEM_CALLBACK 1
+
+#ifdef CONFIG_TOUCH_ELEM_EVENT
+/* Slider event handler task */
+static void slider_handler_task(void *arg)
+{
+    (void) arg; //Unused
+    touch_elem_message_t element_message;
+    while (1) {
+        /* Waiting for touch element messages */
+        if (touch_element_message_receive(&element_message, portMAX_DELAY) == ESP_OK) {
+            if (element_message.element_type != TOUCH_ELEM_TYPE_SLIDER) {
+                continue;
+            }
+            /* Decode message */
+            const touch_slider_message_t *slider_message = touch_slider_get_message(&element_message);
+            if (slider_message->event == TOUCH_SLIDER_EVT_ON_PRESS) {
+                ESP_LOGI(TAG_Slider, "Slider Press, position: %"PRIu32, slider_message->position);
+            } else if (slider_message->event == TOUCH_SLIDER_EVT_ON_RELEASE) {
+                ESP_LOGI(TAG_Slider, "Slider Release, position: %"PRIu32, slider_message->position);
+            } else if (slider_message->event == TOUCH_SLIDER_EVT_ON_CALCULATION) {
+                ESP_LOGI(TAG_Slider, "Slider Calculate, position: %"PRIu32, slider_message->position);
+            }
+        }
+    }
+}
+#elif CONFIG_TOUCH_ELEM_CALLBACK
+/* Slider callback routine */
+void slider_handler(touch_slider_handle_t out_handle, touch_slider_message_t *out_message, void *arg)
+{
+    (void) arg; //Unused
+    if (out_handle != slider_handle) {
+        return;
+    }
+    if (out_message->event == TOUCH_SLIDER_EVT_ON_PRESS) {
+        ESP_LOGI(TAG_Slider, "Slider Press, position: %"PRIu32, out_message->position);
+    } else if (out_message->event == TOUCH_SLIDER_EVT_ON_RELEASE) {
+        ESP_LOGI(TAG_Slider, "Slider Release, position: %"PRIu32, out_message->position);
+    } else if (out_message->event == TOUCH_SLIDER_EVT_ON_CALCULATION) {
+        ESP_LOGI(TAG_Slider, "Slider Calculate, position: %"PRIu32, out_message->position);
+    }
+}
+#endif
+
+
 
 static const char TAG[] = "FW_MAIN";
 
@@ -747,4 +825,38 @@ void app_main()
 
 	debug_set_wifi_logging();
 	ESP_LOGI(TAG,"Initial set up done");
+
+  /* Initialize Touch Element library */
+  touch_elem_global_config_t global_config = TOUCH_ELEM_GLOBAL_DEFAULT_CONFIG();
+  ESP_ERROR_CHECK(touch_element_install(&global_config));
+  ESP_LOGI(TAG_Slider, "Touch element library installed");
+
+  touch_slider_global_config_t slider_global_config = TOUCH_SLIDER_GLOBAL_DEFAULT_CONFIG();
+  ESP_ERROR_CHECK(touch_slider_install(&slider_global_config));
+  ESP_LOGI(TAG_Slider, "Touch slider installed");
+  /* Create Touch slider */
+  touch_slider_config_t slider_config = {
+      .channel_array = channel_array,
+      .sensitivity_array = channel_sens_array,
+      .channel_num = (sizeof(channel_array) / sizeof(channel_array[0])),
+      .position_range = 101
+  };
+  ESP_ERROR_CHECK(touch_slider_create(&slider_config, &slider_handle));
+  /* Subscribe touch slider events (On Press, On Release, On Calculation) */
+  ESP_ERROR_CHECK(touch_slider_subscribe_event(slider_handle,
+                                                 TOUCH_ELEM_EVENT_ON_PRESS | TOUCH_ELEM_EVENT_ON_RELEASE | TOUCH_ELEM_EVENT_ON_CALCULATION, NULL));
+#ifdef CONFIG_TOUCH_ELEM_EVENT
+    /* Set EVENT as the dispatch method */
+    ESP_ERROR_CHECK(touch_slider_set_dispatch_method(slider_handle, TOUCH_ELEM_DISP_EVENT));
+    /* Create a handler task to handle event messages */
+    xTaskCreate(&slider_handler_task, "slider_handler_task", 4 * 1024, NULL, 5, NULL);
+#elif CONFIG_TOUCH_ELEM_CALLBACK
+    /* Set CALLBACK as the dispatch method */
+    ESP_ERROR_CHECK(touch_slider_set_dispatch_method(slider_handle, TOUCH_ELEM_DISP_CALLBACK));
+    /* Register a handler function to handle event messages */
+    ESP_ERROR_CHECK(touch_slider_set_callback(slider_handle, slider_handler));
+#endif
+  ESP_LOGI(TAG_Slider, "Touch slider created");
+  touch_element_start();
+  ESP_LOGI(TAG_Slider, "Touch element library start");
 }
